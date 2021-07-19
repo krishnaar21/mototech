ff1fa3568b27a078c692803c49cacc95c4f0c6df
SphericalKat <amolele@gmail.com>
2021-07-19 15:10:04 +0530

    feat(home): implement about us header
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-			<Header />
+      <Header/>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Logo from "../drawables/logo.svg";
+import AboutBike from "../drawables/about-bike.svg";
 import HeaderBg from "../drawables/header-bg.webp";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="flex flex-col w-full h-full relative">
-      <div className="image-crop bg-burnt-orange pb-4 h-full w-full">
+      <div className="z-0 image-crop bg-burnt-orange pb-4 h-full w-full">
         <img
           id="header-bg"
           className="z-0 relative w-full h-full object-cover image-crop border-b-2 border-black"
@@ -14,25 +15,34 @@ function Header() {
           alt=""
         />
       </div>
-      <div className="flex z-10 absolute pt-12 px-24">
-        <img src={Logo} alt="" />
-      </div>
 
-      <div className="absolute w-1/2 h-full flex flex-col justify-center items-start px-24">
-        <h2 className="font-poppins text-burnt-orange text-4xl font-bold pb-2">
-          SMOOTH RIDES,
-        </h2>
-        <h1 className="font-tomorrow text-white font-semibold text-7xl pb-8">
-          GUARANTEED!
-        </h1>
-        <p className="font-poppins text-light-grey text-2xl pb-12">
-          Power and precision to keep your vehicle fit and make your ride
-          better.
-        </p>
+      {/* body container */}
+      <div className="absolute w-1/2 h-full flex flex-col z-10 items-start px-24">
+        <div className="flex z-10 pt-12">
+          <img src={Logo} alt="" />
+        </div>
 
-        <button className="font-tomorrow font-medium text-white text-xl bg-burnt-orange px-4 py-2 rounded-none">
-          CONTACT US
-        </button>
+        <div className="flex flex-col flex-1 justify-center items-start">
+          <h2 className="font-poppins text-burnt-orange text-4xl font-bold pb-2">
+            SMOOTH RIDES,
+          </h2>
+          <h1 className="font-tomorrow text-white font-semibold text-7xl pb-8">
+            GUARANTEED!
+          </h1>
+          <p className="font-poppins text-light-grey text-2xl pb-12">
+            Power and precision to keep your vehicle fit and make your ride
+            better.
+          </p>
+
+          <button className="font-tomorrow font-medium text-white text-xl bg-burnt-orange px-4 py-2 rounded-none">
+            CONTACT US
+          </button>
+        </div>
+
+        <div className="flex flex-col h-1/4 w-full justify-center items-start">
+          <img src={AboutBike} alt="" className="h-2/5" />
+					<h2 className="text-4xl text-burnt-orange font-tomorrow pt-4">ABOUT US</h2>
+        </div>
       </div>
     </div>
   );

1d85aac758ca6b87ab9f6088d7102ebece076adc
SphericalKat <amolele@gmail.com>
2021-07-19 18:00:35 +0530

    feat(footer): finish desktop layout implementation
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from './components/About'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="flex flex-col justify-center items-center w-full">
       <Header/>
       <About/>
+			<Footer />
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,64 @@
 import React from "react";
 
+import './Footer.css'
+
+import Logo from "../drawables/logo.svg";
+import Instagram from "../drawables/instagram.svg";
+import Facebook from "../drawables/facebook.svg";
+import FooterBike from "../drawables/footer-bike.webp";
+
 function Footer() {
   return (
-    <div>
-      hi owo
-      <div></div>
+    <div
+      className="flex flex-col w-full justify-between items-center p-24 footer-bg"
+      style={{ height: "70%", backgroundImage: `url(${FooterBike})` }}
+    >
+      <h2 className="font-poppins text-white text-5xl">
+        <div>
+          <span className="text-burnt-orange">”</span>It’s not about the
+          destination.
+        </div>
+        <div>
+          It’s about the ride!<span className="text-burnt-orange">“</span>
+        </div>
+      </h2>
+
+      <div className="bottom-0 left-0 flex w-full justify-evenly items-start pt-16">
+        <div className="flex flex-col flex-1">
+          <img src={Logo} alt="" />
+          <p className="font-poppins text-white text-2xl pt-8">
+            Visit us for reliable repairs, superior service, and creative
+            customisation.
+          </p>
+        </div>
+        <div className="flex-1 mx-16">
+          <h2 className="font-tomorrow font-bold text-burnt-orange text-4xl">
+            Address
+          </h2>
+          <p className="font-poppins text-white text-2xl pt-8">
+            1-26-22/1, Subash Nagar, Gollaguda Kaman, Secunderabad, Telangana
+            500015.
+          </p>
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-col">
+            <h2 className="font-tomorrow font-bold text-burnt-orange text-4xl">
+              Contact us
+            </h2>
+            <p className="font-poppins text-white text-2xl pt-8">
+              +91 91004 02098
+            </p>
+
+            <h2 className="font-tomorrow font-bold text-burnt-orange text-4xl pt-16">
+              Follow us
+            </h2>
+            <div className="flex pt-8">
+              <img src={Instagram} alt="" />
+              <img src={Facebook} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

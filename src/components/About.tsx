559860bfa49d1de2304d9b49c123296d9d68d010
SphericalKat <amolele@gmail.com>
2021-07-24 21:54:35 +0530

    feat: add testimonials
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,6 +15,7 @@ import Aprilia from "../drawables/aprilialogo.svg";
 import Jawa from "../drawables/jawalogo.svg";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import Testimonial from "./Testimonial";
 
 function About() {
   return (
@@ -131,25 +132,56 @@ function About() {
         client satisfaction. Here’s what our clients have to say about their
         experience with us.
       </p>
-      <Carousel autoPlay infiniteLoop interval={5000}>
-        <div className="flex flex-col items-start">
-          <p className="font-poppins pt-4 text-xl">
-            “5 stars for the services and the way they attend to the customers.
-            Mainly the owner, Jeevan Reddy. Best and trusted service center.”
-          </p>
-          <p className="font-poppins pt-4 text-xl font-bold">
-            - <span className="text-burnt-orange">Amir MA</span>
-          </p>
-        </div>
-        <div className="flex flex-col items-start">
-          <p className="font-poppins pt-4 text-xl">
-            “5 stars for the services and the way they attend to the customers.
-            Mainly the owner, Jeevan Reddy. Best and trusted service center.”
-          </p>
-          <p className="font-poppins pt-4 text-xl font-bold">
-            - <span className="text-burnt-orange">Amir MA</span>
-          </p>
-        </div>
+      <Carousel autoPlay infiniteLoop interval={5000} showArrows={false} showStatus={false}>
+        <Testimonial
+          body="5 stars for the services and the way they attend to the customers.
+            Mainly the owner, Jeevan Reddy. Best and trusted service center."
+          name="Amir MA"
+        />
+        <Testimonial
+          body="I just gave my Passion XPro for general service. Nice friendly customer service."
+          name="Moses Paul"
+        />
+        <Testimonial
+          body="Excellent and root level service. In the year 2017 I have faced engine problem in my ten year old bike but after enquiring in authorized showroom they informed me it as major problem and given estimation cost of Rs 14000/- to repair the bike with only three months guarantee, then as a second opinion I've met Mototech multi brand bike service for the first time and met Mr.jeevan and showed my bike,  he had resolved my bike engine issue in a much much reasonable price and still my bike is working fine without any issues and with increased engine smoothness. Sincere staff, No cheating and good at receiving customers. Thank you."
+          name="Sandeep Nethi"
+        />
+				<Testimonial
+          body="Nice staff and they are good at servicing."
+          name="Prakhar Pandey"
+        />
+				<Testimonial
+          body="One of the best bike service points. I loved the transparency shown by Mr. Jeevan, the owner.. Each n every detail was shared by him, and the best point the service cost was so amazing and economic.Thank you Mr. Jeevan"
+          name="Nish Vines"
+        />
+				<Testimonial
+          body="Excellent work done and nice reception. Impressed by the work done and the cost was economical. I truly recommend this."
+          name="Vajeed Abdul"
+        />
+				<Testimonial
+          body="Experienced mechanics with good service and value for your time with on time service."
+          name="Ruthvik Raj"
+        />
+				<Testimonial
+          body="The team is really good with their service and delivers the vehicle on time."
+          name="Allen Joseph"
+        />
+				<Testimonial
+          body="Give them the bike and have it done your way. Loyal people with no fake business work. It's been two years since I have been trusting them."
+          name="Chanakya Sharma"
+        />
+				<Testimonial
+          body="Best place for all your two-wheeler repairs. I would strongly recommend everyone to visit this place. Thank you for the service"
+          name="Flint Eastwood Thomas"
+        />
+				<Testimonial
+          body="Always been more than satisfied with my delivery. I take all my friends' bikes to him as well. They're that great!"
+          name="Johnson D’souza"
+        />
+				<Testimonial
+          body="If you need your two-wheeler serviced this is the place to go!!"
+          name="Vanam Mahesh Kumar"
+        />
       </Carousel>
     </div>
   );
---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface TestimonialProps {
+  body: string;
+  name: string;
+}
+
+function Testimonial({body, name}: TestimonialProps) {
+  return (
+    <div className="flex flex-col items-start">
+      <p className="font-poppins pt-4 text-xl text-left">
+        “{body}”
+      </p>
+      <p className="font-poppins pt-4 text-xl font-bold">
+        - <span className="text-burnt-orange">{name}</span>
+      </p>
+    </div>
+  )
+}
+
+export default Testimonial

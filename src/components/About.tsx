4e684527aa63c1156f36d0ffb8f50e252e5e79f2
SphericalKat <amolele@gmail.com>
2021-07-25 22:46:06 +0530

    refactor(about): sort testimonials by length
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -188,54 +188,53 @@ function About() {
             }
           }}
         >
-          <Testimonial
-            body="5 stars for the services and the way they attend to the customers.
-            Mainly the owner, Jeevan Reddy. Best and trusted service center."
-            name="Amir MA"
-          />
-          <Testimonial
-            body="I just gave my Passion XPro for general service. Nice friendly customer service."
-            name="Moses Paul"
-          />
-          <Testimonial
+					<Testimonial
             body="Excellent and root level service. In the year 2017 I have faced engine problem in my ten year old bike but after enquiring in authorized showroom they informed me it as major problem and given estimation cost of Rs 14000/- to repair the bike with only three months guarantee, then as a second opinion I've met Mototech multi brand bike service for the first time and met Mr.jeevan and showed my bike,  he had resolved my bike engine issue in a much much reasonable price and still my bike is working fine without any issues and with increased engine smoothness. Sincere staff, No cheating and good at receiving customers. Thank you."
             name="Sandeep Nethi"
-          />
-          <Testimonial
-            body="Nice staff and they are good at servicing."
-            name="Prakhar Pandey"
           />
           <Testimonial
             body="One of the best bike service points. I loved the transparency shown by Mr. Jeevan, the owner.. Each n every detail was shared by him, and the best point the service cost was so amazing and economic.Thank you Mr. Jeevan"
             name="Nish Vines"
           />
           <Testimonial
+            body="Give them the bike and have it done your way. Loyal people with no fake business work. It's been two years since I have been trusting them."
+            name="Chanakya Sharma"
+          />
+					<Testimonial
+            body="5 stars for the services and the way they attend to the customers. Mainly the owner, Jeevan Reddy. Best and trusted service center."
+            name="Amir MA"
+          />
+          <Testimonial
+            body="Best place for all your two-wheeler repairs. I would strongly recommend everyone to visit this place. Thank you for the service"
+            name="Flint Eastwood Thomas"
+          />
+					 <Testimonial
             body="Excellent work done and nice reception. Impressed by the work done and the cost was economical. I truly recommend this."
             name="Vajeed Abdul"
           />
           <Testimonial
+            body="Always been more than satisfied with my delivery. I take all my friends' bikes to him as well. They're that great!"
+            name="Johnson D’souza"
+          />
+					<Testimonial
             body="Experienced mechanics with good service and value for your time with on time service."
             name="Ruthvik Raj"
+          />
+					<Testimonial
+            body="I just gave my Passion XPro for general service. Nice friendly customer service."
+            name="Moses Paul"
           />
           <Testimonial
             body="The team is really good with their service and delivers the vehicle on time."
             name="Allen Joseph"
           />
           <Testimonial
-            body="Give them the bike and have it done your way. Loyal people with no fake business work. It's been two years since I have been trusting them."
-            name="Chanakya Sharma"
-          />
-          <Testimonial
-            body="Best place for all your two-wheeler repairs. I would strongly recommend everyone to visit this place. Thank you for the service"
-            name="Flint Eastwood Thomas"
-          />
-          <Testimonial
-            body="Always been more than satisfied with my delivery. I take all my friends' bikes to him as well. They're that great!"
-            name="Johnson D’souza"
-          />
-          <Testimonial
             body="If you need your two-wheeler serviced this is the place to go!!"
             name="Vanam Mahesh Kumar"
+          />
+					<Testimonial
+            body="Nice staff and they are good at servicing."
+            name="Prakhar Pandey"
           />
         </Carousel>
       </div>
